53074827294e5a44edbe534c0c2a1df55a155fbd
Ifeoluwa Odewale <ifeoluwa.odewale@gmail.com>
2023-03-25 17:59:48 +0000

    Remove unsed code from the test file and devconatainer config files

--- dpk.test.js
+++ dpk.test.js
@@ -1,9 +1,4 @@
-const { deterministicPartitionKey } = require("./dpk");
-const crypto = require("crypto"); 
-
-const encryptData = (stringifyData) => {
-  return crypto.createHash("sha3-512").update(stringifyData).digest("hex");
-}
+const { deterministicPartitionKey, getEncryptData, getStringifyData } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
@@ -14,7 +9,7 @@ describe("deterministicPartitionKey", () => {
   it("Returns the encryption of the value when given a string input", () => {
     const data = "event";
     const partitionKey = deterministicPartitionKey(data);
-    expect(partitionKey).toBe(encryptData(JSON.stringify(data)))
+    expect(partitionKey).toBe(getEncryptData(getStringifyData(data)))
   });
 
   it("Returns the partitionKey when given an object with partitionKey key", () => {
@@ -26,16 +21,16 @@ describe("deterministicPartitionKey", () => {
   it("Returns the encryption of the object value given an object without partitionKey key", () => {
     const event = {nonePartitionKey: "event"}
     const partitionKey = deterministicPartitionKey(event);
-    expect(partitionKey).toBe(encryptData(JSON.stringify(event)))
+    expect(partitionKey).toBe(getEncryptData(getStringifyData(event)))
   });
 
 
   it("Returns the encryption of the object value given an object without partitionKey key whose length is more than 256", () => {
     const event = {nonePartitionKey: "Duis lacinia tristique euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi laoreet eros non tincidunt imperdiet. Vivamus rutrum, massa nec condimentum sagittis, neque quam lacinia quam, sed viverra purus diam id urna. Praesent id dui varius, maximus felis vel, vulputate odio. Praesent semper consequat erat sed hendrerit. Vivamus consequat fermentum eros at finibus."}
-    const stringifyEvent = JSON.stringify(event);
+    const stringifyEvent = getStringifyData(event);
     const partitionKey = deterministicPartitionKey(event);
     expect(stringifyEvent.length).toBeGreaterThan(256)
-    expect(partitionKey).toBe(encryptData(stringifyEvent))
+    expect(partitionKey).toBe(getEncryptData(stringifyEvent))
   });
 
 });
